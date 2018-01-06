6f05f00b56d899dde8f4254fa6f46dd0d351f8cc
DESKTOP-D1HEJHO\dkhovrich <khovrych.d@gmail.com>
2018-01-06 12:48:36 +0200

    EventEmitter, network

---
+++ eventEmitter/server.js
@@ -0,0 +1,40 @@
+const EventEmitter = require('events');
+
+class Server extends EventEmitter {
+    constructor(client) {
+        super();
+        process.nextTick(() => {
+            this.emit('response', 'Server started');
+        });
+        client.on('command', (command, args) => {
+            switch (command) {
+                case 'help':
+                case 'add':
+                case 'ls':
+                case 'delete':
+                    this[command](args);
+                    break;
+                default:
+                    this.emit('response', 'Unknown command...');
+            }
+        });
+    }
+
+    help() {
+        this.emit('response', 'help...');
+    }
+
+    add(args) {
+        this.emit('response', args.join(' '));
+    }
+
+    ls() {
+        this.emit('response', 'ls...');
+    }
+
+    delete() {
+        this.emit('response', 'delete...');
+    }
+};
+
+module.exports = (client) => new Server(client);
